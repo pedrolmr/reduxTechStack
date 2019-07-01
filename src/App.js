bb07f5aa9a5948a8cfebc74dbb3ac7fff4079edc
pedrolmr <pedroluismontesinosrivera@gmail.com>
2019-06-30 20:21:34 -0400

    used the connect function to the LibraryList file but still not passing correct jsx data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import LibraryList from './components/LibraryList'
+
+import { Header } from './components/common'
 
 const App = () => {
     return(
         <Provider store={createStore(reducers)}>
-            <View/>
+            <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+                <View style={{ flex: 1}}>
+                    <Header headerText="Tech Stack"/>
+                    <LibraryList />
+                </View>
+            </SafeAreaView>
         </Provider>
     )
 }
